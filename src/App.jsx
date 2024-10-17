16300602a0e32d378f87619688561e03762957d7
akash100a <arakash968786@gmail.com>
2024-10-17 12:50:48 +0600

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  let [allproduct,setAllProduct] = useState([]);
   useEffect(() => {
     let getData = async () => {
       let blobs = await fetch("https://techstorevercelapp.vercel.app/akash/api/products");
       let res = await blobs.json();
       let newdata = await res.data.products;
-      console.log(newdata);
+      setAllProduct(newdata);      
+      // console.log();
       
     }
     getData();
   }, [])
   return (
     <div>
-      
+      {
+        allproduct.map((val,index)=>(
+          <div className="" key={index}>
+            <h1 className='white'>{val.title}</h1>
+            <img src={val.thumbnail} alt="" />
+          </div>
+        ))
+      }
 
     </div>
   )
